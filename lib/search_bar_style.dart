6341bc39726bdad52393ee8b819e6fd5aa84198e
Cosmin Marin <cosmin.didis@gmail.com>
2020-04-18 13:36:49 +0300

    ---

--- lib/search_bar_style.dart
+++ lib/search_bar_style.dart
@@ -5,11 +5,12 @@ class SearchBarStyle {
   final EdgeInsetsGeometry padding;
   final BorderRadius borderRadius;
   final BoxBorder border;
+  final int height;
 
   const SearchBarStyle(
-      {
-        this.backgroundColor = const Color.fromRGBO(142, 142, 147, .15),
+      {this.backgroundColor = const Color.fromRGBO(142, 142, 147, .15),
       this.padding = const EdgeInsets.all(5.0),
-        this.border = const Border.fromBorderSide(BorderSide(color: Colors.black12)),
-      this.borderRadius: const BorderRadius.all(Radius.circular(5.0))});
+      this.border = const Border.fromBorderSide(BorderSide(color: Colors.black12)),
+      this.borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+      this.height: 80});
 }
